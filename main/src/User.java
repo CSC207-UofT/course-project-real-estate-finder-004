fb77f3fd25b113a27c69a50f70d38732fd5f1d9c
Sebastian Clancy <foldit.com@gmail.com>
2021-10-16 18:21:58 -0400

    Added username to User class.

--- main/src/User.java
+++ main/src/User.java
@@ -1,14 +1,24 @@
 public class User {
     private String name;
+    private String username;
     private String email;
     private String phone;
     private String password;
 
-    public User(String name, String email, String phone, String password) {
+    public User(String name, String username, String email, String phone, String password) {
         this.name = name;
+        this.username = username;
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
