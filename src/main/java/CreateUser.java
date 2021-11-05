e6a323a39eb54f421c1890b389b6ec127f7e6061
jalukaharsh <jalukaharsh@gmail.com>
2021-11-05 15:07:31 -0400

    Added constructor and method to add user.

--- src/main/java/CreateUser.java
+++ src/main/java/CreateUser.java
@@ -1,6 +1,12 @@
 public class CreateUser {
+    private UserStorage userStorage;
 
-    public static void main(String name, String username, String email, String phone, String password) {
-        User
+    public CreateUser(UserStorage my_user_storage) {
+        this.userStorage = my_user_storage;
+    }
+
+    public void createUser(String name, String username, String email, String phone, String password){
+        User newUser = new User(name, username, email, phone, password);
+        this.userStorage.addUser(newUser);
     }
 }
