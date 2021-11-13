f092a7a4989b1ab596e1f6c09cf0480baf1858ec
alexistassone <alexis.tassone@mail.utoronto.ca>
2021-11-13 14:31:54 -0500

    - added LoginUserTest class - added test cases to the LoginUserTest class

---
+++ src/test/java/LoginUserTest.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class LoginUserTest {
+    @Test (timeout = 50)
+    public void testLoginUser(){
+        Buyer b = new Buyer("Alexis Tassone", "alexistassone", "alexis.tassone@mail.utoronto.ca",
+                "1234567890", "1234");
+        LoginUser myLogin = new LoginUser();
+        assertTrue(myLogin.loginUser(b, "1234"));
+        assertFalse(myLogin.loginUser(b, "4321"));
+    }
+}
