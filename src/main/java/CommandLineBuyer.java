d084ff0693a98333c832e2148fde63258b0f1fa4
jalukaharsh <jalukaharsh@gmail.com>
2021-11-14 10:44:29 -0500

    Lots of changes. Implemented template design pattern for user interfaces (created abstract UserInterface class). Wrote runInterface, which is a structure all UIs should follow for this program. Abstractly defined methods in UserInterface. Added child classes for CommandLine depending on whether seller or buyer. Seller methods fixed, buyer methods to be implemented.

---
+++ src/main/java/CommandLineBuyer.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CommandLineBuyer extends CommandLine {
+
+    public CommandLineBuyer(InputStream input, UserStorage users) {
+        super(input, users);
+    }
+
+    public void choicesUser(Buyer user) {
+        // TODO: to be implemented
+    }
+
+}
