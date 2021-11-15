59fcc5f3443b86400e236dd624ad39bfb7f80f14
Sebastian Clancy <foldit.com@gmail.com>
2021-11-15 02:00:38 -0500

    Fixed LoginUserTest

--- src/test/java/LoginUserTest.java
+++ src/test/java/LoginUserTest.java
@@ -4,18 +4,18 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class LoginUserTest {
-    @Test (timeout = 50)
+    @Test ()
     public void testLoginUser(){
         DatabaseManager myDatabaseManager = new DatabaseManager();
         myDatabaseManager.signUp("Alexis Tassone", "b","alexistassone", "alexis.tassone@mail.utoronto.ca",
                 "1234567890", "1234");
         try {
-            User logged_in_user = myDatabaseManager.loginUser("Alexis Tassone", "1234");
+            User logged_in_user = myDatabaseManager.loginUser("alexistassone", "1234");
         } catch(LoginUserNotFoundException | LoginWrongPasswordException e) {
             fail();
         }
         try{
-            User myUser = myDatabaseManager.loginUser("Alexis Tassone", "4321");
+            User myUser = myDatabaseManager.loginUser("alexistassone", "4321");
             fail();
         } catch (LoginUserNotFoundException | LoginWrongPasswordException ignored) {
 
