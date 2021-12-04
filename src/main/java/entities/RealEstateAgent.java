4e582f2993947a0c448f051b294167235284871c
Sebastian Clancy <foldit.com@gmail.com>
2021-12-04 14:51:56 -0500

    Added docs for RealEstateAgent.

--- src/main/java/entities/RealEstateAgent.java
+++ src/main/java/entities/RealEstateAgent.java
@@ -7,23 +7,27 @@ import entities.Seller;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-// <<<<<<< Serializable_added
 public class RealEstateAgent implements Serializable {
     private ArrayList<Buyer> buyers = new ArrayList<Buyer>();
-// =======
-// public class entities.RealEstateAgent {
-//     private ArrayList<entities.Buyer> buyers = new ArrayList<>();
-// >>>>>>> main
     private Seller client;
     private Property property;
 
+    /**
+     * Constructor for the RealEstateAgent.
+     *
+     * @param client The Seller object that this RealEstateAgent corresponds to.
+     */
     public RealEstateAgent(Seller client) {
         this.client = client;
     }
 
-    /** This function is called when a entities.Buyer is interested in a property and would like to speak to the entities.Seller.
+    /**
+     * This function is called when a entities.Buyer is interested in a property and would like to speak to the entities.Seller.
      * The function sends a message to the seller that contains the entities.Buyer's contact details and
      * informs the entities.Seller that the entities.Buyer is interested.
+     *
+     * @param custom_message A String with the message that the buyer wants to be passed along.
+     * @param buyer          The Buyer that wants this message passed to the client.
      */
     public void connectBuyersAndSeller(String custom_message, Buyer buyer) {
         // Not yet working.... Need to figure out how to communicate with it.
@@ -32,14 +36,23 @@ public class RealEstateAgent implements Serializable {
                 buyer.getPhone() + ". Here is a message from the prospective buyer: " + custom_message);
     }
 
+    /**
+     * Returns the number of Buyers that are associated with this RealEstateAgent.
+     *
+     * @return An int with the length of the buyers object.
+     */
     public int getNumberOfBuyers() {
         return buyers.size();
     }
 
+    /**
+     * Adds a buyer to the list of buyers associated with this RealEstateAgent.
+     *
+     * @param buyer The Buyer instance to be added.
+     */
     public void addBuyer(Buyer buyer) {
         this.buyers.add(buyer);
     }
-
 
 
 }
