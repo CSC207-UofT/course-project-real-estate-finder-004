043f4b0f1e8ce91f5b11103737baab49628a797b
Mojan Majid <mojan.majid@mail.utoronto.ca>
2021-12-04 14:33:47 -0500

    Documentation for the exceptions and the HashMap Storages

--- src/main/java/externalinterfaces/Storage.java
+++ src/main/java/externalinterfaces/Storage.java
@@ -1,7 +1,22 @@
 package externalinterfaces;
 
 public interface Storage<T, K> {
+    /**
+     * Adds an object to the storage.
+     * @param object    object to be added to the storage that has an id as its key
+     */
     void add(K object);
+
+    /**
+     * Removes an id and its associated object from the storage
+     * @param id    a key that has an associated value, object
+     */
     void remove(T id);
+
+    /**
+     * Returns an object in the storage given its key.
+     * @param id    a key that has an associated value, object
+     * @return  an object in the storage
+     */
     K get(T id);
 }
