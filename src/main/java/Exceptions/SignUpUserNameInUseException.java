cf7d5aee5e4ce39ef53f4d026d330cc266cbd8ee
alexistassone <alexis.tassone@mail.utoronto.ca>
2021-12-08 12:29:39 -0500

    Added documentation to SignUpEmailInUseException, SignUpPhoneNumberInUseException, and SignUpUserNameInUseException

--- src/main/java/Exceptions/SignUpUserNameInUseException.java
+++ src/main/java/Exceptions/SignUpUserNameInUseException.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
 public class SignUpUserNameInUseException extends IllegalArgumentException{
+    /**
+     * Creates an exception to indicate that a user name used by a user when signing up is already associated with another user.
+     */
     public SignUpUserNameInUseException() {super("User name is already in use.");}
 }
