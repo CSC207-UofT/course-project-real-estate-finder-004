6daaec721ad339326e73b238760bcd916ba2523f
Sebastian Clancy <foldit.com@gmail.com>
2021-12-08 16:43:00 -0500

    Temporary commit with prop ids added for the userUpdate method in DatabaseManager.

--- src/test/java/TestDataPersistence.java
+++ src/test/java/TestDataPersistence.java
@@ -4,7 +4,10 @@ import entities.Seller;
 public class TestDataPersistence {
     public static void main(String[] args) {
         DatabaseManager manager = new DatabaseManager();
-        Seller me = (Seller) manager.loginUser("sebastian", "1234");
+        Seller me = (Seller) manager.loginUser("sebastian", "pass");
+//        System.out.println(me.getEmail());
+//        me.setEmail("sebastian@gmail.com");
+        System.out.println(me.getUnreadMessages());
         me.addMessage("TEST");
         System.out.println(me.getUnreadMessages());
 
@@ -12,7 +15,7 @@ public class TestDataPersistence {
 
 
         DatabaseManager manager2 = new DatabaseManager();
-        Seller me2 = (Seller) manager2.loginUser("sebastian", "1234");
+        Seller me2 = (Seller) manager2.loginUser("sebastian", "pass");
         System.out.println(me2.getUnreadMessages());
     }
 }
