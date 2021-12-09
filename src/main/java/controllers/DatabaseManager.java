02c8c1dd4f90fc9103685045f319ebca4e1eeb08
Sebastian Clancy <foldit.com@gmail.com>
2021-12-08 20:40:06 -0500

    Removed Default User and properties.

--- src/main/java/controllers/DatabaseManager.java
+++ src/main/java/controllers/DatabaseManager.java
@@ -210,8 +210,8 @@ public class DatabaseManager {
         this.agentCreator = new AgentCreator(userStorage, userStorageReadWriter, propertyStorage, propertyStorageReadWriter, agentStorage, agentStorageReadWriter);
         this.userCreator = new UserCreator(userStorage, userStorageReadWriter, propertyStorage, propertyStorageReadWriter, agentStorage, agentStorageReadWriter);
 
-        this.userCreator.create("John Smith","s", "jsmith", "1234@gmail.com", "1234567890", "1234");
-        this.propertyCreator.create((Seller) this.userStorage.get("jsmith"), "6 Hoskin Avenue", "Toronto", "Ontario", "CA", "M5T 2HY", 16000F, 1000, true);
-        this.agentCreator.create((Seller) this.userStorage.get("jsmith"));
+//        this.userCreator.create("John Smith","s", "jsmith", "1234@gmail.com", "1234567890", "1234");
+//        this.propertyCreator.create((Seller) this.userStorage.get("jsmith"), "6 Hoskin Avenue", "Toronto", "Ontario", "CA", "M5T 2HY", 16000F, 1000, true);
+//        this.agentCreator.create((Seller) this.userStorage.get("jsmith"));
     }
 }
